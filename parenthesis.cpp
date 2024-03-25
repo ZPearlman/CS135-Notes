83876719b8c987315efd32352c1ec10b0b7456ba
ZPearlman <156953128+ZPearlman@users.noreply.github.com>
2024-03-25 18:32:50 +0000

    Fixed parenthesis.cpp to work

--- parenthesis.cpp
+++ parenthesis.cpp
@@ -3,8 +3,26 @@
 
 using namespace std;
 
-template <typename T> void parenChecker(Stack<T> paren){
-    for (int i = 0; i < count - 1; i++){
-        
+int main(){
+    Stack<char> s;
+    string eq = "(5+10(2) / 13(6-1))";
+    
+    try{
+        for(int i = 0; i < eq.length(); i++){
+        if(eq[i] == '(')
+            s.push('(');
+        if(eq[i] == ')')
+            s.pop();
+        }
+
+        if(s.getCount())
+            cout << "Bad" << endl;
+        else
+            cout << "Good" << endl;
     }
+    catch(out_of_range e){
+        cout << "Bad" << endl;
+    }
+
+    return 0;
 }
